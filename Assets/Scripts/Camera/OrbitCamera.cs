baf85926ef3310b43534c6e9cdae1629255bc911
Kurt Shumaker <kshumak@gmail.com>
2017-12-09 01:28:38 -0800

    Added comment for some new camera code
    Implemented alpha reduction of objects that are between the camera and the player.
    
    - Promptly commented the code out, because it's slow and it looks terrible.
    
    - Left it in commented to keep the idea around.

--- Assets/Scripts/Camera/OrbitCamera.cs
+++ Assets/Scripts/Camera/OrbitCamera.cs
@@ -29,6 +29,13 @@ namespace Assets.Scripts.Camera
         private const float UP_ANGLE = 315.0f;
 
         private Transform target; // target for camera transform
+        //private List<GameObject> affected; // list of objects with reduced alphas
+
+
+
+        // ====================================================================
+        // *                              START                               *
+        // ====================================================================
 
         private void Start()
         {
@@ -51,7 +58,16 @@ namespace Assets.Scripts.Camera
             target.gameObject.SetActive(false);
             target.position = this.transform.position;
             target.rotation = this.transform.rotation;
+
+            // Initialize list of game objects affected by camera behavior
+            //affected = new List<GameObject>();
         }
+
+
+
+        // ====================================================================
+        // *                         CAMERA MOVEMENT                          *
+        // ====================================================================
 
         public void controlInterface(ButtonPresses btns)
         {
@@ -76,7 +92,50 @@ namespace Assets.Scripts.Camera
 
             // Store player's new location
             playerLastLoc = UGen.getTop(player);
+
+            //reduceAlpha();
         }
+
+
+        /* THIS LOOKS TERRIBLE AND TAKES A LOT OF PROCESSING TIME
+        // ====================================================================
+        // *                        CLIP BEHAVIOR                             *
+        // ====================================================================
+
+        private const float ALPHA_REDUCTION = 0.25f;
+        private const float ALPHA_RETURN = 1.0f;
+        private const int LAYER_MASK = Int32.MaxValue ^ (int)UGen.eLayerMask.IGNORE_RAYCAST
+            ^ (int)UGen.eLayerMask.WATER ^ (int)UGen.eLayerMask.UI ^ (int)UGen.eLayerMask.PLAYER
+            ^ (int)UGen.eLayerMask.NPC ^ (int)UGen.eLayerMask.ENEMY;
+
+        private void reduceAlpha() {
+            // Calculate direction and distance of raycast
+            Vector3 direction = UGen.getTop(player) - this.transform.position;
+            float distance = Vector3.Distance(UGen.getTop(player), this.transform.position);
+
+            // Get all objects directly between the camera and the player
+            RaycastHit[] hits = Physics.RaycastAll(this.transform.position, direction, distance, LAYER_MASK);
+
+            // Return all affected objects to normal opacity
+            foreach (GameObject obj in affected) {
+                Color col = obj.GetComponent<Renderer>().material.color;
+                col.a = ALPHA_RETURN;
+                obj.GetComponent<Renderer>().material.color = col;
+            }
+
+            // Clear List
+            affected.Clear();
+
+            // Add all the hits to affected objects list and reduce opacity
+            foreach (RaycastHit hit in hits) {
+                hit.collider.GetComponent<Renderer>().material.EnableKeyword("_ALPHABLEND_ON");
+                Color col = hit.collider.GetComponent<Renderer>().material.color;
+                col.a = ALPHA_REDUCTION;
+                hit.collider.GetComponent<Renderer>().material.color = col;
+                affected.Add(hit.collider.gameObject);
+            }
+        }
+        */
 
     } // close class
 } // close namespace
