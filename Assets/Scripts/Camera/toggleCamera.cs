d2dc327833d901bb982c99a56ca15847b593c3bb
Justin <RhinoBabies@users.noreply.github.com>
2017-12-14 17:58:53 -0800

    Tomo ending stuff

---
+++ Assets/Scripts/Camera/toggleCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleCamera : MonoBehaviour {
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+    private void turnCamOnOff()
+    {
+        if (cam.enabled == true)
+            cam.enabled = false;
+        else
+            cam.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Levels/startEndScene.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class startEndScene : MonoBehaviour {
+
+    private Camera cam;
+    private int totalEnemies;
+    public Image fadeToBlack;
+    private Color bgOrigColor;
+    private Color transAlpha;
+    private Color opaqAlpha;
+    private Color currColor;
+
+    private void Start()
+    {
+        totalEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        bgOrigColor = fadeToBlack.color;
+
+        transAlpha = Color.black;
+        opaqAlpha = Color.black;
+
+        transAlpha.a = 0.0f;
+        opaqAlpha.a = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        while (totalEnemies > 0)
+        {
+            totalEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+            Debug.Log("Enemy Count: " + totalEnemies);
+        }
+
+        //detects no more enemies
+        //fade to black
+
+        fadeToBlack.color = transAlpha;
+        while (fadeToBlack.color.a < opaqAlpha.a) {
+            currColor.a = fadeToBlack.color.a;
+            currColor.a += .05f;
+            fadeToBlack.color = currColor;
+        }
+
+        levelLoader.lvlLoader.loadNextScene("NikoTomo");
+        //load dialogue scene with Tomo and Niko
+    }
+}
